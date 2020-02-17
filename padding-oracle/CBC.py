936bd89ba63a604b6d4cfa506f6193b46284f494
sunmoyi <s897949163@gmail.com>
2020-02-17 16:59:33 +0800

    paddingOracle.v1
    第一版本，时间匆忙，赶工完成

---
+++ padding-oracle/CBC.py
@@ -0,0 +1,65 @@
+from Crypto.Cipher import AES
+from binascii import a2b_hex, b2a_hex
+
+def add_to_16_by_number(text):
+    add = 16 - (len(text.encode('utf-8')) % 16)
+    text = text.encode('utf-8')
+    text = text + add.to_bytes(length=1, byteorder='big', signed=False) * add
+    return text
+
+
+# 如果text不足16位的倍数就用空格补足为16位
+def add_to_16(text):
+    if len(text.encode('utf-8')) % 16:
+        add = 16 - (len(text.encode('utf-8')) % 16)
+    else:
+        add = 0
+    text = text + ('\0' * add)
+    return text.encode('utf-8')
+
+
+# 加密函数
+def encrypt(iv, text):
+    key = '9999999999999999'.encode('utf-8')
+    mode = AES.MODE_CBC
+    text = add_to_16_by_number(text)
+    cryptos = AES.new(key, mode, iv)
+    cipher_text = cryptos.encrypt(text)
+    return cipher_text
+
+
+# 解密后，去掉补足的空格用strip() 去掉
+def decrypt(iv, text):
+    key = '9999999999999999'.encode('utf-8')
+    mode = AES.MODE_CBC
+    cryptos = AES.new(key, mode, iv)
+    plain_text = cryptos.decrypt(text)
+    # print(plain_text)
+    return plain_text
+    # return bytes.decode(plain_text).rstrip('\0')
+
+
+def check(iv, string):
+    text = decrypt(iv, string)
+    number = text[-1]
+    if number > 16 or number == 0:
+        return text, False
+
+    flag = True
+    for i in range(number * -1, -1):
+        if text[i] != number:
+            # print("text[{0}]".format(i) + str(text[i]))
+            flag = False
+    return text, flag
+
+
+if __name__ == '__main__':
+    iv = b'1234567887654321'
+    e = encrypt(iv, "abcdefghijklmnopqrstuvw")
+    d = check(iv, e)
+    print("加密:", e)
+    print("解密:", d)
+
+    # string = 'abcdefghi'
+    # print(add_to_16_by_number(string))
+    # print(add_to_16(string))
---
+++ padding-oracle/main.py
@@ -0,0 +1,83 @@
+import CBC
+
+
+def findB(iv, c1, c2):
+    for i in range(16):
+        c1_change = c1[:i] + (c1[i] ^ 1).to_bytes(length=1, byteorder='big', signed=False) + c1[i + 1:]
+        C = c1_change + c2
+        # print(CBC.check(iv, C))
+        _, ans = CBC.check(iv, C)
+        if not ans:
+            return 16 - i
+    return 0
+
+
+def makeB(iv, c1):
+    print(c1)
+    for i in range(256):
+        c1_change = c1[:-1] + i.to_bytes(length=1, byteorder='big', signed=False)
+        # print(c1_change)
+        if CBC.check(iv, c1_change)[1]:
+            print(CBC.check(iv, c1_change))
+            return
+    return c1
+
+def calculatec2(iv, c1, c2, b):
+    # print(c1)
+    # print(CBC.check(iv, c1 + c2))
+    m2 = bytes()
+    for i in range(15 - b, -1, -1):
+        c1_end = bytes()
+        for k in range(i + 1, 16):
+            c1_end = c1_end + (c1[k] ^ (16 - i) ^ (15 - i)).to_bytes(length=1, byteorder='big', signed=False)
+        # print(c1_end)
+        for j in range(256):
+            c1_change = c1[:i] + j.to_bytes(length=1, byteorder='big', signed=False) + c1_end
+            # print(len(c1_change))
+            if CBC.check(iv, c1_change + c2)[1]:
+                # print(CBC.check(iv, c1_change + c2))
+                m2 = (j ^ (16 - i) ^ c1[i]).to_bytes(length=1, byteorder='big', signed=False) + m2
+                c1 = c1_change
+            pass
+    return m2
+
+
+def calculatec1(iv, c1):
+    # print(CBC.check(iv, iv + c1))
+    iv_end = bytes()
+    ans = bytes()
+    for i in range(256):
+        iv_change = iv[:-1] + i.to_bytes(length=1, byteorder='big', signed=False)
+        if CBC.check(iv, iv_change + c1)[1]:
+            # print(CBC.check(iv, iv_change + c1))
+            iv_end = (0x01 ^ i ^ iv[-1]).to_bytes(length=1, byteorder='big', signed=False)
+            # print(iv_end)
+            ans = calculatec2(iv_change, iv_change, c1, 1)
+            # print(ans)
+            break
+        # print(c1_change)
+    # print(ans + iv_end)
+    return ans + iv_end
+
+
+def paddingOracle(iv, C):
+    c1 = C[:16]
+    c2 = C[-16:]
+    m2 = c2
+    b = findB(iv, c1, c2)
+    # print("b = {0}".format(b))
+    if b != 16:
+        m2 = calculatec2(iv, c1, c2, b)
+    m1 = calculatec1(iv, c1)
+
+    return m1 + m2
+
+
+if __name__ == '__main__':
+    iv = b'1234567887654321'
+    M = 'test for padding-oracle'
+    if len(M) < 16 or len(M) >=32:
+        print("M error")
+    else:
+        C = CBC.encrypt(iv, M)
+        print(paddingOracle(iv, C))
